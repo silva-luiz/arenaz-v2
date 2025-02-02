2e0ece447ad1d5d05704aaacff0d452d43566031
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-02-02 16:45:37 -0300

    feat: 404 page created

--- arena_z/src/components/error/ErrorPage.jsx
+++ arena_z/src/components/error/ErrorPage.jsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import styles from '../error/ErrorPage.module.css';
+import NavbarSite from "../NavbarSite";
+import SiteFooter from "../SiteFooter";
+import errorImg from '../../../public/404_img.jpg'; // Certifique-se do caminho correto da imagem
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className={styles.container}>
+      <NavbarSite className={styles.navbar}/>
+      <div className={styles.errorContainer}>
+        <img src={errorImg} alt="Logo" className={styles.errorImg} />
+        <div className={styles.errorMessageContainer}>
+          <h1 className={styles.heading}>404</h1>
+          <p className={styles.message}>Ops! Não foi possível encontrar essa página</p>
+          <Link to="/" className={styles.link}>Voltar para a página inicial</Link>
+        </div>
+      </div>
+      <SiteFooter />
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
