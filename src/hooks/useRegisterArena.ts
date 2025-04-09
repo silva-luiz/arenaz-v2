2ba3d439cf83ce39ee40995544ae08b6b71805a3
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-04-09 17:17:41 -0300

    whitespace removed

--- src/hooks/useRegisterArena.ts
+++ src/hooks/useRegisterArena.ts
@@ -4,7 +4,7 @@ export const useRegisterArena = (url) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const registerArena = async (arenaData) => { 
+  const registerArena = async (arenaData) => {
     const token = sessionStorage.getItem("auth-token");
 
     if (!token) {
