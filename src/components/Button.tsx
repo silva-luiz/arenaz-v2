fa04a6bc877ceaa37b04fe6e1ba753d36a7d000b
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-04-10 13:54:46 -0300

    button component changed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,12 @@ interface IButton {
   text: string;
   className: string;
   handleClick?: (e: any) => Promise<void>;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const Button = ({ text, className, handleClick }: IButton) => {
+const Button = ({ text, className, handleClick, type = 'button' }: IButton) => {
   return (
-    <button className={className} onClick={handleClick}>
+    <button className={className} onClick={handleClick} type={type}>
       {text}
     </button>
   );
