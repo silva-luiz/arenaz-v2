e00950f7483c763806fb1404ddfff5c240022b00
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-04-10 23:09:31 -0300

    pagination removed

--- src/components/Reservations/ReservationsPage.tsx
+++ src/components/Reservations/ReservationsPage.tsx
@@ -2,8 +2,8 @@ import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
 import styles from '../Reservations/ReservationsPage.module.scss';
 
-let active = 1;
-let items = [];
+const  active = 1;
+const items = [];
 for (let number = 1; number <= 5; number++) {
   items.push(
     <Pagination.Item key={number} active={number === active}>
@@ -126,11 +126,6 @@ const ReservationsPage = () => {
             ))}
           </tbody>
         </Table>
-        <div>
-          <Pagination className={styles.reservationPagination}>
-            {items}
-          </Pagination>
-        </div>
       </div>
     </div>
   );
