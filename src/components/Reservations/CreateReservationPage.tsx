82e2dd34c2c8c9dd30711a9c0f0a2bed7ac2e90c
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-04-12 15:18:30 -0300

    create reservation page started

--- src/components/Reservations/CreateReservationPage.tsx
+++ src/components/Reservations/CreateReservationPage.tsx
@@ -9,6 +9,8 @@ import Form from 'react-bootstrap/Form';
 import styles from '../Reservations/CreateReservationPage.module.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import { Link } from 'react-router-dom';
+import Button from 'components/Button';
 
 registerLocale('pt-BR', ptBR);
 
@@ -41,80 +43,136 @@ const CreateReservationPage = () => {
 
   return (
     <div>
-      <h2>Informações da Arena</h2>
+      <h2 className={styles.pageTitle}>Informações da Arena</h2>
       <div className={styles.arenaInfosContainer}>
         <div className={styles.singleInfoContainer}>
-          <h4>Nome da Arena</h4>
+          <h4 className={styles.arenaInfo}>Nome da Arena</h4>
           <p>Arena name 1</p>
         </div>
         <div className={styles.singleInfoContainer}>
-          <h4>Categoria</h4>
+          <h4 className={styles.arenaInfo}>Categoria</h4>
           <p className={styles.arenaCategory}>Society</p>
         </div>
         <div className={styles.singleInfoContainer}>
-          <h4>Valor / hora</h4>
+          <h4 className={styles.arenaInfo}>Valor / hora</h4>
           <p>R$ 100/hora</p>
         </div>
       </div>
 
-      <h2>Datas e horários disponíveis</h2>
-      <p>
+      <h2 className={styles.pageTitle}>Datas e horários disponíveis</h2>
+      <p className={styles.pageSubtitle}>
         Escolha o dia, horário de início e horário de término para o seu
         agendamento:
       </p>
 
       <div className={styles.arenaInfosContainer}>
         <div className={styles.reservationContainer}>
-          <div className={styles.timePickerContainer}>
-            <h4>Escolha a data</h4>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              dateFormat="dd/MM/yyyy"
-              locale="pt-BR"
-              minDate={new Date()}
-              className="form-control"
-              wrapperClassName={styles.datePickerWrapper}
-            />
-          </div>
+          <form action="" className={styles.reservationForm}>
+            <div className={styles.timePickerContainer}>
+              <h4 className={styles.arenaInfo}>Data da reserva</h4>
 
-          <div className={styles.selectTimeContainer}>
-            <div className={styles.timePickerContainer}>
-              <h4>Horário de início</h4>
-              <Form.Select
-                aria-label="Selecione o horário de início"
-                onChange={handleStartTimeChange}
-                value={startTime}
-              >
-                <option value="">Selecione um horário...</option>
-                {timeOptions.map((time) => (
-                  <option key={time} value={time}>
-                    {time}
-                  </option>
-                ))}
-              </Form.Select>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="dd/MM/yyyy"
+                locale="pt-BR"
+                minDate={new Date()}
+                className={styles.datePickerInput}
+                wrapperClassName={styles.datePickerWrapper}
+              />
             </div>
-            <div className={styles.timePickerContainer}>
-              <h4>Horário de saída</h4>
-              <Form.Select
-                aria-label="Selecione o horário de término"
-                onChange={handleEndTimeChange}
-                value={endTime}
-                disabled={!startTime}
-              >
-                <option value="">Selecione um horário...</option>
-                {timeOptions
-                  .filter((time) => time > startTime)
-                  .map((time) => (
-                    <option key={time} value={time}>
+
+            <div className={styles.selectTimeContainer}>
+              <div className={styles.timePickerContainer}>
+                <h4 className={styles.arenaInfo}>Horário de início</h4>
+                <Form.Select
+                  aria-label="Selecione o horário de início"
+                  onChange={handleStartTimeChange}
+                  value={startTime}
+                  className={styles.selectTime}
+                >
+                  <option value="" className={styles.optionDefault}>
+                    Selecione um horário...
+                  </option>
+                  {timeOptions.map((time) => (
+                    <option
+                      key={time}
+                      value={time}
+                      className={styles.optionTime}
+                    >
                       {time}
                     </option>
                   ))}
-              </Form.Select>
-            </div>
-          </div>
+                </Form.Select>
+              </div>
 
-          <button className="primaryButton">Prosseguir para agendamento</button>
+              <div className={styles.timePickerContainer}>
+                <h4 className={styles.arenaInfo}>Horário de saída</h4>
+                <Form.Select
+                  aria-label="Selecione o horário de término"
+                  onChange={handleEndTimeChange}
+                  value={endTime}
+                  disabled={!startTime}
+                  className={styles.selectTime}
+                >
+                  <option value="" className={styles.optionDefault}>
+                    Selecione um horário...
+                  </option>
+                  {timeOptions
+                    .filter((time) => time > startTime)
+                    .map((time) => (
+                      <option
+                        key={time}
+                        value={time}
+                        className={styles.optionTime}
+                      >
+                        {time}
+                      </option>
+                    ))}
+                </Form.Select>
+              </div>
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label className={styles.inputLabel}>Jogador responsável</label>
+              <div className={styles.inputWrapper}>
+                <input
+                  type="text"
+                  placeholder="Nome"
+                  name="responsiblePlayer"
+                  className={styles.inputText}
+                />
+              </div>
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label className={styles.inputLabel}>Telefone para contato</label>
+              <div className={styles.inputWrapper}>
+                <input
+                  type="text"
+                  placeholder="Telefone para contato"
+                  name="contactPhone"
+                  className={styles.inputText}
+                />
+              </div>
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label className={styles.inputLabel}>Valor (R$)</label>
+              <div className={styles.inputWrapper}>
+                <input
+                  type="text"
+                  placeholder="Valor"
+                  name="price"
+                  className={styles.inputText}
+                />
+              </div>
+            </div>
+
+            <div className={styles.formActions}>
+              <button className="primaryButton">Confirmar agendamento</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
